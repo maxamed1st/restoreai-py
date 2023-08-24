6cf98a384f03e89403e41cb620ba0305bc026c6c
maxamed1st <maxamed1st@icloud.com>
2023-08-24 16:50:34 +0100

    initialize Eventhub and even subscription

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import auth, restore
+from PubSub import EventHub
 from inference import Restore
 
 app = FastAPI()
@@ -9,6 +10,8 @@ app.include_router(restore.router)
 
 @app.on_event("startup")
 async def subscribe_to_events():
+    #initialize EventHub
+    EventHub()
     #initialize event subscriptions
     Restore()
 
